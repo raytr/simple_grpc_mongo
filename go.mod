c826220d71bfbd65bff3732bcf883bb71c639018
nghiatran <nghia.tran@f6galaxy.com>
2022-05-02 22:20:24 +0700

    update mod

--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.1 // indirect
@@ -23,8 +22,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
-	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
-	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220429170224-98d788798c3e // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"context"
+	"net/http"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/kaysush/protobuf-demo/proto/echo" //<-- Take a note that I've created my code folder in $GOPATH/src
+	pb "github.com/grpc-ecosystem/grpc-gateway/v2/examples/internal/helloworld"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
 
 func main() {
-	req := &echo.EchoRequest{Name: "Sushil"}
-	data, err := proto.Marshal(req)
+	ctx := context.TODO()
+	mux := runtime.NewServeMux()
+	// Register generated routes to mux
+	err := pb.RegisterGreeterHandlerServer(ctx, mux, &GreeterServer{})
 	if err != nil {
-		log.Fatalf("Error while marshalling the object : %v", err)
+		panic(err)
 	}
-
-	res := &echo.EchoRequest{}
-	err = proto.Unmarshal(data, res)
+	// Register custom route for  GET /hello/{name}
+	err = mux.HandlePath("GET", "/hello/{name}", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		w.Write([]byte("hello " + pathParams["name"]))
+	})
 	if err != nil {
-		log.Fatalf("Error while un-marshalling the object : %v", err)
+		panic(err)
 	}
-	fmt.Printf("Value from un-marshalled data is %v", res.GetName())
+	http.ListenAndServe(":8080", mux)
+}
 
+// GreeterServer is the server API for Greeter service.
+type GreeterServer struct {
+}
+
+// SayHello implement to say hello
+func (h *GreeterServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		Message: "hello " + req.Name,
+	}, nil
+}
+
+func (s *serviceServer) Check(ctx context.Context, in *health.HealthCheckRequest) (*health.HealthCheckResponse, error) {
+	return &health.HealthCheckResponse{Status: health.HealthCheckResponse_SERVING}, nil
 }
